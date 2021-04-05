1a9d94e968657be0d0e535205bac6fe7de22cea0
HP <logeswari5>
2021-04-05 14:51:53 +0530

    Add the borrower field in Bookinstance

--- catalog/admin.py
+++ catalog/admin.py
@@ -25,6 +25,8 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+
     list_filter = ('status', 'due_back')
 
     fieldsets = (
@@ -32,6 +34,6 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back','borrower')
         }),
         )
