2ced44fc1584ac0f15cc03b081c5f4b8de3cd08d
HP <logeswari5>
2021-04-05 15:20:21 +0530

    Bug fixes

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 def index(request):
     # Generate counts of some of the main objects
@@ -31,3 +33,11 @@ class BookListView(generic.ListView):
     paginate_by = 2
 class BookDetailView(generic.DetailView):
     model = Book
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
